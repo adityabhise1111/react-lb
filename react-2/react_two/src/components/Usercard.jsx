e02ae2385a28276fe8adbfa992fc26e25af8f851
aditya <adityabhise1111@gmail.com>
2025-02-17 01:31:55 +0530

    nik

--- react-2/react_two/src/components/Usercard.jsx
+++ react-2/react_two/src/components/Usercard.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-
-import reactLogo from '../assets/react.svg'
-
+import reactLogo from '../assets/nikam.jpg'
 
 const userCard = () => {
   return (
     <div className='user-container'>
-        <h1 id="user-name" > Aditya Nikam</h1>
+        <p id="user-name" > Aditya Nikam</p>
         <img id="user-img"  src={reactLogo} alt="Nikam" />
         <p id="user-info"> Extreamly hardworking</p>
-    </div>
+    </div>    
   )
 }
-
 export default userCard
